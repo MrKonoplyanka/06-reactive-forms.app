255e38ae5fb2f186e0e303293e4ccd6fe4a47f77
Iván Rilo Ferreiro <ivanriloferreiro@gmail.com>
2025-10-28 11:55:47 +0100

    Fin sección 14

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,17 +1,8 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
-export class FormUtils{
-  static isValidField(form: FormGroup, fieldName: string): boolean | null {
-    return (
-      !!form.controls[fieldName].errors && form.controls[fieldName].touched
-    )
+export class FormUtils {
 
-  }
-  static getFieldError(form: FormGroup, fieldName: string): string | null {
-    if (!form.controls[fieldName]) return null;
-
-    const errors = form.controls[fieldName].errors ?? {};
-
+  static getTextError(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -25,4 +16,45 @@ export class FormUtils{
     }
     return null;
   }
+
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return (
+      !!form.controls[fieldName].errors && form.controls[fieldName].touched
+    )
+
+  }
+
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
+
+    const errors = form.controls[fieldName].errors ?? {};
+
+    return FormUtils.getTextError(errors);
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+
+    )
+
+  }
+
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+
+    if (!formArray.controls[index]) return null;
+    const errors = formArray.controls[index].errors ?? {};
+    return FormUtils.getTextError(errors);
+  }
+
+  static onSave(form:FormGroup) {
+     if(form.invalid){
+      form.markAllAsTouched();
+      return;
+    }
+
+    form.reset();
+  }
+
 }

01591b0c6028e908bde59ae9d8eb203520891980
MrKonoplyanka <ivanriloferreiro@gmail.com>
2025-10-28 20:05:51 +0100

    Fin forms

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,6 +1,19 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import { timeout } from 'rxjs';
 
+export async function sleep() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2500);
+  })
+}
 export class FormUtils {
+
+
+  static namePattern = /^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ]+(?:\s+[A-Za-zÁÉÍÓÚÜÑáéíóúüñ]+)+$/;
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
   static getTextError(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
@@ -12,6 +25,18 @@ export class FormUtils {
 
         case 'min':
           return `Valor mínimo de ${errors['min'].min}`;
+
+        case 'email':
+          return `El valor ingresado no es un correo electrónico`
+
+        case 'emailTaken':
+        return `El correo electrónico no está disponible`
+
+        case 'strider':
+          return `El nombre no puede ser strider`
+
+        default:
+          return 'Error de validación no controlado'
       }
     }
     return null;
@@ -48,13 +73,42 @@ export class FormUtils {
     return FormUtils.getTextError(errors);
   }
 
-  static onSave(form:FormGroup) {
-     if(form.invalid){
+  static onSave(form: FormGroup) {
+    if (form.invalid) {
       form.markAllAsTouched();
       return;
     }
 
-    form.reset();
+    form.reset('');
+
+
+  }
+
+  static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true }
+    }
+  }
+
+  static async checkingServerResponse(control: AbstractControl): Promise<ValidationErrors | null> {
+
+    await sleep();
+    const formValue = control.value;
+
+    if(formValue === 'hola@mundo.com'){
+      return{
+        emailTaken:true
+      };
+    }
+    return null;
+  }
+
+  static notStrider(control:AbstractControl):ValidationErrors|null{
+
+    return control.value === 'strider' ? {strider:true} : null;
   }
 
 }
